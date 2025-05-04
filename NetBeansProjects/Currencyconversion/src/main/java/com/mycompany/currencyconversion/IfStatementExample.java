f0d38954b7b377400c98b0f0e08d845b95b4ddf0
24035383 <felixfelianderkamonewa@gmail.com>
2025-05-04 18:32:56 +0200

    first commit

---
+++ NetBeansProjects/Currencyconversion/src/main/java/com/mycompany/currencyconversion/IfStatementExample.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.currencyconversion;
+
+/**
+ *
+ * @author Tsheg
+ */
+public class IfStatementExample {
+    public static void main(String[] args) {
+        int number = 10;
+
+        // Using an if statement to check if the number is positive
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        }
+
+        // Using an if statement to check if the number is zero
+        if (number == 0) {
+            System.out.println("The number is zero.");
+        }
+
+        // Using an if statement to check if the number is negative
+        if (number < 0) {
+            System.out.println("The number is negative.");
+        }
+    }
+}
+    
+
