32eddc3553f961aa3b10977c01ee4df0bf63c57a
24035383 <felixfelianderkamonewa@gmail.com>
2025-05-12 13:42:41 +0200

    first commit

---
+++ NetBeansProjects/biancaKAMUNEWA/src/main/java/com/mycompany/biancakamunewa/BiancaKAMUNEWA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.biancakamunewa;
+
+/**
+ *
+ * @author Tsheg
+ */
+public class BiancaKAMUNEWA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ NetBeansProjects/mphoyanga/src/main/java/com/mycompany/mphoyanga/Mphoyanga.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mphoyanga;
+
+/**
+ *
+ * @author Tsheg
+ */
+public class Mphoyanga 
+
+
+    {
+
+    public static void main(String[] args){
+       displayLoveNote();   
+       
+    }
+    public static void displayLoveNote() {
+        
+        System.out.println("I LOVE YOU KAMONEWA");
+        System.out.println("my person mcwaa");
+        System.out.println("XOXO");
+    }
+}
+
+
